b56456ad373a16c4fe0aca10c59d021931c83149
Aaron Wolen <aaron@wolen.com>
2022-07-26 08:48:38 -0500

    Tests for SparseNdArray creation

---
+++ apis/r/tests/testthat/test_SOMASparseNdArray.R
@@ -0,0 +1,11 @@
+test_that("SOMASparseNdArray creation", {
+  uri <- withr::local_tempdir("sparse-ndarray")
+  ndarray <- SOMASparseNdArray$new(uri)
+  ndarray$create(arrow::int32(), shape = c(10, 10))
+
+  expect_equal(tiledb::tiledb_object_type(uri), "ARRAY")
+  expect_equal(ndarray$dimnames(), c("__dim_0", "__dim_1"))
+
+  expect_equal(ndarray$attrnames(), "data")
+  expect_equal(tiledb::datatype(ndarray$attributes()$data), "INT32")
+})
